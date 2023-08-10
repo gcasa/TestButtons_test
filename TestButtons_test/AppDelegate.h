d9ef4d1836fdab7c2b86f233c8572a487ceaf990
Gregory Casamento <greg.casamento@gmail.com>
2023-08-10 09:55:12 -0400

    Initial Commit

---
+++ TestButtons_test/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  TestButtons_test
+//
+//  Created by Gregory John Casamento on 8/10/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ TestButtons_test/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  TestButtons_test
+//
+//  Created by Gregory John Casamento on 8/10/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
